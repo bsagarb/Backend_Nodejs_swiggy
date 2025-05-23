b872fdc34178a54acdbd5cbf45300e29b9f64da1
Bongu Sagar <121047104+bsagarb@users.noreply.github.com>
2025-05-23 21:04:50 +0530

    Update Vendor.js unique email commited

--- models/Vendor.js
+++ models/Vendor.js
@@ -8,7 +8,7 @@ const vendorSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        // unique: true
     },
     password: {
         type: String,
