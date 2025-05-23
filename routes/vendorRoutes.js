e5f7fecf3ad7522d53bf9dd8a202572b5388515e
Bongu Sagar <121047104+bsagarb@users.noreply.github.com>
2025-05-23 19:54:24 +0530

    Update vendorRoutes.js with instaregister

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,9 +1,10 @@
-const {vendorRegister,vendorLogin,getAllVendors,getVendorById} = require('../controllers/vendorController');
+const {vendorRegister,vendorLogin,getAllVendors,getVendorById,instaRegister } = require('../controllers/vendorController');
 const express = require('express');
 
 const router = express.Router();
 
 router.post('/register', vendorRegister);
+router.post('/instaregister', instaRegister);
 router.post('/login',vendorLogin);
 router.get('/all-vendors', getAllVendors);
 router.get('/single-vendor/:id', getVendorById)
